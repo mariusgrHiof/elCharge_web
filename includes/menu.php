5ac8ce3b61385efa0d306996bd37b60242f76fec
ukon1990 <mail@jonaskf.net>
2016-03-03 08:08:51 +0100

    lagt til index.php + meny

---
+++ includes/menu.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonas
+ * Date: 03.03.16
+ * Time: 08:05
+ */
+?>
+<nav>
+    <ul>
+        <li>Meny element</li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonas
+ * Date: 03.03.16
+ * Time: 07:57
+ */
+
+?>
+
+<head>
+    <title>elCharge</title>
+    <meta charset="utf-8"/>
+</head>
+<body>
+<header>
+    <h1>elCharge</h1>
+</header>
+<?php //Website navigation
+    include 'includes/menu.php'
+?>
+<div>
+
+</div>
+</body>
