2d26e9e8235466b9657ac544b5f569cff6899b59
Jonas <mail@jonaskf.net>
2016-03-08 10:28:04 +0100

    Kan nå laste ned fil fra nobil med php for statisk data.

--- ReadStuff.php
+++ ReadStuff.php
@@ -4,7 +4,14 @@
  * User: jonas
  * Date: 07.03.2016
  * Time: 12.03
- */?>
+ */
+$url = "http://nobil.no/api/server/search.php?apikey=274b68192b056e268f128ff63bfcd4a4&apiversion=3&action=search&type=near&lat=59.91673&long=10.74782&distance=2000&limit=10";
+$fileName ="datadump.json";
+if(!file_exists($fileName)){
+    //Downloading data dump if not already downloaded
+    file_put_contents($fileName, fopen($url, 'r'));
+}
+?>
 
 
 <html>
@@ -17,6 +24,8 @@
     </script>
 </head>
 <body>
-
+    <?php
+        include $fileName;
+    ?>
 </body>
 </html>
