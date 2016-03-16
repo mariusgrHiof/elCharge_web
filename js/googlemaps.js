8e061fd6e3a0505cf7619b6959d41806e02afac4
Jonas <mail@jonaskf.net>
2016-03-16 10:54:29 +0100

    filtrere basert på enkelt type lade ting

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -5,7 +5,7 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 59.91673, lng: 10.74782},
-        zoom: 12
+        zoom: 6
 
     });
     generateMarkers();
