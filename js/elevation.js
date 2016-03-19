8f66c75caf65dd0d919cac17148c8b7949e25ee9
Jonas <mail@jonaskf.net>
2016-03-19 23:52:37 +0100

    urls

--- js/elevation.js
+++ js/elevation.js
@@ -5,5 +5,8 @@
 var elevationService;
 
 function getElevation(latlng){
+    //https://developers.google.com/maps/documentation/elevation/intro#ElevationResponses
+    //https://developers.google.com/maps/documentation/javascript/elevation#Elevation
+    //https://developers.google.com/maps/documentation/javascript/examples/map-latlng-literal
     //https://maps.googleapis.com/maps/api/elevation/json?locations=39.7391536,-104.9847034&key=AIzaSyAijAKyJWxMHEodrkA3jD2psiz6LmI0hT8
 }
