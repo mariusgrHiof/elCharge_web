af2fa3ee4793fb98bf9e07483e6890762977e4bc
Jonas <mail@jonaskf.net>
2016-04-06 22:27:17 +0200

    jQuery lagt inn igjen. hvordan ble den borte?

--- js/addStations.js
+++ js/addStations.js
@@ -92,19 +92,6 @@ var connectors = new Array();
  * @param callback
  */
 
-function readJsonFile(callback){
-    // Gotten from: http://stackoverflow.com/questions/19706046/how-to-read-an-external-local-json-file-in-javascript
-    var path = "data/datadump.json";
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", path, true);
-    rawFile.onreadystatechange = function() {
-        if(rawFile.readyState == 4 && rawFile.status == "200"){
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
 
 /* usage
  readTextFile(function(text){
