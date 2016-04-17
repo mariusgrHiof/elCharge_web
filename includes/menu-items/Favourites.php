eff5dc0710880c25371a38ed8ddb18fb641f8e36
Jonas <mail@jonaskf.net>
2016-04-17 22:53:58 +0200

    Forsøk på fiksing på problemet som gjør at skjermen er scrollbar sidlengs på mobil.

--- includes/menu-items/Favourites.php
+++ includes/menu-items/Favourites.php
@@ -1,4 +1,4 @@
-<h2 class="menu-item caret">Favoritter</h2>
+<h2 class="menu-item caret clear-both">Favoritter</h2>
 <div class="sub-item">
     <em>Dine favoritt ladestasjoner. Kun synlig for innloggede brukere.</em>
 </div>
