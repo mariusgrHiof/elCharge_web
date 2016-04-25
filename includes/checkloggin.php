2123967d3dfa136691a74b08566509e59be244e0
supremetaco <mariusgr@hiof.no>
2016-04-25 13:11:57 +0200

    teste mysql connection

--- includes/checkloggin.php
+++ includes/checkloggin.php
@@ -41,6 +41,30 @@ try {
     } else {
         echo "Feil brukernavn eller passord";
     }*/
+    $_SESSION['sessId'] = rand(1,99);
+    $mysqli = mysqli_init();
+    if (!$mysqli) {
+        die('mysqli_init failed');
+    }
+
+    if (!$mysqli->options(MYSQLI_INIT_COMMAND, 'SET AUTOCOMMIT = 0')) {
+        die('Setting MYSQLI_INIT_COMMAND failed');
+    }
+
+    if (!$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5)) {
+        die('Setting MYSQLI_OPT_CONNECT_TIMEOUT failed');
+    }
+
+    if (!$mysqli->real_connect($servername, $username, $password, $username)) {
+        die('Connect Error (' . mysqli_connect_errno() . ') '
+            . mysqli_connect_error());
+    }
+
+    echo 'Success... ' . 'Din sessid ' . $_SESSION['sessId'] . $mysqli->host_info . "\n";
+
+    $mysqli->close();
+
+
 }
 catch (mysqli_sql_exception $ms){
     echo $ms;
