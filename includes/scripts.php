9dfaeed2914b3de14856be448d6e68100806604a
Jonas <mail@jonaskf.net>
2016-04-26 09:11:15 +0200

    Lasting av lokal fil er funksjonelt for android appen, men ikke for iOS for nå. Test med noen scripts satt som async, men mulig selve funksjonene må settes til async.

--- includes/scripts.php
+++ includes/scripts.php
@@ -2,18 +2,18 @@
 echo '<script type="text/javascript" src="cordova.js"></script>'.
         '<script type="text/javascript" src="js/index.js"></script>';
 addJS('js/include.js', false, false);
-addJS('js/addStations.js', false, false);
-addJS('js/global.js', false, false);
+addJS('js/addStations.js', true, false);
+addJS('js/global.js', true, false);
 addJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyAijAKyJWxMHEodrkA3jD2psiz6LmI0hT8&callback=initMap&sensors=true&libraries=places,visualization,geometry', true, true);
-addJS('js/googlemaps.js', false, false);
-addJS('js/login.js', false, false);
+addJS('js/googlemaps.js', true, false);
+addJS('js/login.js', true, false);
 addJS('js/geolocation-marker.js', false, false);
-addJS('js/chargers-nearby.js', false, false);
+addJS('js/chargers-nearby.js', true, false);
 addJS('js/MarkerClusterer.js', false, false);
-addJS('js/maplayers.js', false, false);
-addJS('js/navigation.js', false, false);
-addJS('js/buttons.js', false, false);
-addJS('js/elevation.js', false, false);
+addJS('js/maplayers.js', true, false);
+addJS('js/navigation.js', true, false);
+addJS('js/buttons.js', true, false);
+addJS('js/elevation.js', true, false);
 
 
 
--- js/global.js
+++ js/global.js
@@ -109,7 +109,7 @@ function downloadDump(){
         },
         type: 'POST',
         //TODO: Check out this URL for persistent storage with phonegap - http://docs.phonegap.com/en/2.5.0/cordova_file_file.md.html
-        url: phonegap ? "http://nobil.no/api/server/datadump.php?apikey=274b68192b056e268f128ff63bfcd4a4&fromdate=2005-01-01&format=json":"datadump.json",
+        url: phonegap && isIOS ? "http://nobil.no/api/server/datadump.php?apikey=274b68192b056e268f128ff63bfcd4a4&fromdate=2005-01-01&format=json":"datadump.json",
         data: {},
         success: function(data){
             console.log("File download completed");
