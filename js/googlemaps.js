a1a632931e03a44e550290009ec9920c052d9461
Jonas <mail@jonaskf.net>
2016-05-04 09:45:19 +0200

    Minnebruk tweaks

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -164,23 +164,9 @@ function setMapOnAll(map) {
 }
 //Deleting all the markers
 function deleteMarkers() {
-    setMapOnAll(null);
     //Memory managenent
-    for(l in markerListeners){
-        delete markerListeners[l];
-    }
-    for(iw in infoWindows){
-        infoWindows[iw].setContent(null)
-        infoWindows[iw] = null;
-        delete infoWindows[iw];
-    }
-    for(m in markers){
-        google.maps.event.clearInstanceListeners(markers[m]);
-        delete markers[m];
-    }
-    markerListeners = [];
-    infoWindows = [];
-    markers = [];
+    setMapOnAll(null);
+    markers.length = 0;
 }
 function centerOnUser(){
     //Refreshing user pos
