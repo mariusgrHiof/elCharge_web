c2a07f1a6d10a1a716979792777ab1eab1911e37
Jonas <mail@jonaskf.net>
2016-06-16 18:50:38 +0200

    moved login bans

--- includes/menu.php
+++ includes/menu.php
@@ -8,34 +8,36 @@
 
 ?>
 <nav class="menu" style="left:-700px;">
-    <?php
-    include 'includes/login.php';
-    include 'includes/userwindow.php';
-    ?>
-    <ul id="menu-list">
-        <li>
-            Velg bilmodell
-            <select id="select-car" name="car-model">
-                <option value="0">Vis alle ladere</option>
-            </select>
-        </li>
-        <li class="border">
-            <?php include 'menu-items/chargers-nearby.html' ?>
-        </li>
-        <li class="border">
-            <?php include 'menu-items/favourites.html' ?>
-        </li>
-        <li class="border">
-            <?php include 'menu-items/route.html' ?>
-        </li>
-        <li class="border">
-            <?php include 'menu-items/filter.html' ?>
-        </li>
-        <li class="border">
-            <?php include 'menu-items/about-us.html' ?>
-        </li>
-        <li class="border">
-            <?php include 'menu-items/help.html' ?>
-        </li>
-    </ul>
+  <ul id="menu-list">
+    <li>
+      <?php
+      include 'includes/login.php';
+      include 'includes/userwindow.php';
+      ?>
+    </li>
+    <li>
+      Velg bilmodell
+      <select id="select-car" name="car-model">
+        <option value="0">Vis alle ladere</option>
+      </select>
+    </li>
+    <li class="border">
+      <?php include 'menu-items/chargers-nearby.html' ?>
+    </li>
+    <li class="border">
+      <?php include 'menu-items/favourites.html' ?>
+    </li>
+    <li class="border">
+      <?php include 'menu-items/route.html' ?>
+    </li>
+    <li class="border">
+      <?php include 'menu-items/filter.html' ?>
+    </li>
+    <li class="border">
+      <?php include 'menu-items/about-us.html' ?>
+    </li>
+    <li class="border">
+      <?php include 'menu-items/help.html' ?>
+    </li>
+  </ul>
 </nav>
