3c0ce44d43e3b78f5a0b60a08e53e9fa25f2c478
ukon1990 <mail@jonaskf.net>
2016-07-05 00:52:06 +0200

    Less DOM interaction in nearby.js and less var declarations

--- js/modules/nearby.js
+++ js/modules/nearby.js
@@ -24,34 +24,36 @@ var nearby = {
     nearby.updateHTML();
   },
   updateHTML : function(){
-    $('#chargers-nearby').html("");
     nearby.chargers.sort(function (a, b){
       return a.distance - b.distance;
     });
-    var thisPos = [];
-    var id;
+    var thisPos = [],
+      id,
+      nationalID,
+      element;
     for(var id in nearby.chargers){
-      var nationalID = nearby.chargers[id].csmd.International_id;
+      nationalID = nearby.chargers[id].csmd.International_id;
       //If it is matching our filters and is available to the public
       if(station.getCarMatch(nationalID)){
         try{
           thisPos = nearby.chargers[id].csmd.Position.replace(/[()]/g,"").split(",");
-          $('#chargers-nearby').append(
-            '<li class="border" style="height:4em; width:auto; padding: 0.5em 0 0.5em 0;">' +
-              '<img class="cover-third float-left img-height-4em" src=\"' + station.getImage(nationalID) + '\"/>' +
-              '<div class="chargePointColor" style="height:4em;background-color:' +
-                (station.list[nationalID].attr.st[21].attrvalid == "1" ? (station.occupiedStatus(nationalID) > 0.4 ? 'lightgreen' : 'yellow') : 'blue') + ';"></div>'+
-              '<div class="cover-twothird float-right" style="width:calc(66% - 1em);">'+
-                '<strong class="float-left station-title"><a class="station" value="' + nationalID +'" href="#">' + nearby.chargers[id].csmd.name + '</a></strong><br />'+
-                '<span>' + nearby.chargers[id].distance.toFixed(2)+ 'km </span>'+
-                '<button class="float-left nav-here tooltip" onclick="navigation.fromUser(this)" value="'+ thisPos +'"><p class="tooltiptext">Naviger hit</p>Naviger hit</button>' +
-                '<div class="clear-both">' +//read-more
-                '</div>' +
+          element = '<li class="border" style="height:4em; width:auto; padding: 0.5em 0 0.5em 0;">' +
+            '<img class="cover-third float-left img-height-4em" src=\"' + station.getImage(nationalID) + '\"/>' +
+            '<div class="chargePointColor" style="height:4em;background-color:' +
+              (station.list[nationalID].attr.st[21].attrvalid == "1" ? (station.occupiedStatus(nationalID) > 0.4 ? 'lightgreen' : 'yellow') : 'blue') + ';"></div>'+
+            '<div class="cover-twothird float-right" style="width:calc(66% - 1em);">'+
+              '<strong class="float-left station-title"><a class="station" value="' + nationalID +'" href="#">' + nearby.chargers[id].csmd.name + '</a></strong><br />'+
+              '<span>' + nearby.chargers[id].distance.toFixed(2)+ 'km </span>'+
+              '<button class="float-left nav-here tooltip" onclick="navigation.fromUser(this)" value="'+ thisPos +'"><p class="tooltiptext">Naviger hit</p>Naviger hit</button>' +
+              '<div class="clear-both">' +//read-more
               '</div>' +
-            '</li>');
+            '</div>' +
+          '</li>';
+
         }catch(e){console.log(e); console.log(nearby.chargers[id]); console.log(id);}
       }
     }
+    $('#chargers-nearby').html(element);
     station.bindStationNames();
   }
 };
