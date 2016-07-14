f2c38051394589f02ef30294e60118152062d80b
Jonas <mail@jonaskf.net>
2016-07-14 11:45:58 +0200

    User registration fix

--- api/register.php
+++ api/register.php
@@ -13,25 +13,29 @@ $sql = "select count(*) from ec_user";
 $result = $conn->query($sql);
 
 if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-        $new_user_id = ($row['count(*)'] == 0 ? $row['count(*)'] : $row['count(*)']);
-    }
+  while($row = $result->fetch_assoc()){
+    $new_user_id = ($row['count(*)'] == 0 ? $row['count(*)'] : $row['count(*)']);
+  }
 }
 if(!empty(($_POST['username'] && $_POST['password'] && $_POST['mail']))){
-    $sql = "INSERT INTO ec_user
-                    VALUES (
-                    " . $new_user_id . ",
-                    '". filter_var(strtolower($_POST['username']), FILTER_SANITIZE_STRING). "',
-                    '". password_hash(filter_var($_POST['password'], FILTER_SANITIZE_STRING), PASSWORD_DEFAULT) . "',
-                    '" . filter_var($_POST['mail'], FILTER_SANITIZE_STRING) . "',
-                    null,
-                    null
-                    )";
-    if ($conn->query($sql) === TRUE) {
-        echo "Registrering fullført";
-    } else {
-        echo "Brukernavn  eller mail er allerede registrert.";
-    }
+  $sql = "INSERT INTO ec_user
+            VALUES (
+            " . $new_user_id . ",
+            '". filter_var(strtolower($_POST['username']), FILTER_SANITIZE_STRING). "',
+            '". password_hash(filter_var($_POST['password'], FILTER_SANITIZE_STRING), PASSWORD_DEFAULT) . "',
+            '" . filter_var($_POST['mail'], FILTER_SANITIZE_STRING) . "',
+            '{}',
+            null,
+            null
+            )";
+  if ($conn->query($sql) === TRUE) {
+    echo "Registrering fullført";
+  } else {
+    echo $sql;
+    echo "Brukernavn  eller mail er allerede registrert.";
+  }
+}else{
+  echo 'Alle feltene var ikke fyllt ut';
 }
 $conn->close();
 ?>
