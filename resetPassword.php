5ae1832ac0d49673f6cc873c878e560354daf2a8
Jonas <mail@jonaskf.net>
2016-08-05 15:32:27 +0200

    PHPMailer test

--- resetPassword.php
+++ resetPassword.php
@@ -1,18 +1,43 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-echo '2';
-try{
-  ini_set("SMTP", "smtp.hiof.no");
-  ini_set("sendmail_from", "no-reply@hiof.no");
+require 'api/PHPMailer/PHPMailerAutoload.php';
+$mail = new PHPMailer(false);
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
 
-  $message = "En test mail";
+$send_using_gmail = true;
 
-  $headers = "From: no-reply@hiof.no";
+$email = 'mail@jonaskf.net';
+$name = 'Jonas K Flønes';
 
-  mail("mail@jonaskf.net", "Testing", $message, $headers);
-  echo "Sendt?<BR/>";
-}catch(Exception $e){
-  echo $e;
+$email_from = 'jossa90@gmail.com';
+$name_from = 'Jonas K Flønes';
+
+//Send mail using gmail
+if($send_using_gmail){
+   $mail->IsSMTP(); // telling the class to use SMTP
+   $mail->SMTPAuth = true; // enable SMTP authentication
+   $mail->SMTPSecure = "ssl"; // sets the prefix to the servier
+   $mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
+   $mail->Port = 465; // set the SMTP port for the GMAIL server
+   $mail->Username = "jossa90@gmail.com"; // GMAIL username
+   $mail->Password = "P4rt1sj0n132"; // GMAIL password
 }
+//Typical mail data
+$mail->AddAddress($email, $name);
+$mail->SetFrom($email_from, $name_from);
+$mail->Subject = "Snasen kaffe";
+$mail->Body = "Kreft smaker godt";
+
+try{
+   $mail->Send();
+   echo "Success! \r\nE=" . $mail->ErrorInfo;
+} catch(Exception $e){
+//Something went bad
+  echo "Fail - " . $mail->ErrorInfo;
+}
+?>
