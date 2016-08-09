9fe36119e395038fd3bfab143f0ab083a052a823
Jonas <mail@jonaskf.net>
2016-08-09 21:59:37 +0200

    test

--- resetPassword.php
+++ resetPassword.php
@@ -9,33 +9,44 @@ $mail->SMTPOptions = array(
     )
 );
 
+$email = 'mail@jonaskf.net';
+$name = 'Jonas K F';
 $send_using_gmail = true;
 
-$email = 'mail@jonaskf.net';
-$name = 'Jonas K Flønes';
-
-$email_from = 'jossa90@gmail.com';
-$name_from = 'Jonas K Flønes';
-
-//Send mail using gmail
+//SMTP
 if($send_using_gmail){
-   $mail->IsSMTP(); // telling the class to use SMTP
-   $mail->SMTPAuth = true; // enable SMTP authentication
-   $mail->SMTPSecure = "ssl"; // sets the prefix to the servier
-   $mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
-   $mail->Port = 465; // set the SMTP port for the GMAIL server
-   $mail->Username = "jossa90@gmail.com"; // GMAIL username
-   $mail->Password = "P4rt1sj0n132"; // GMAIL password
+  $email_from = 'jossa90@gmail.com';
+  $name_from = 'Jonas K Flønes';
+
+  $mail->IsSMTP(); // telling the class to use SMTP
+  $mail->SMTPAuth = true; // enable SMTP authentication
+  $mail->SMTPSecure = "ssl"; // sets the prefix to the servier
+  $mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
+  $mail->Port = 465; // set the SMTP port for the GMAIL server
+  $mail->Username = "jossa90@gmail.com"; // GMAIL username
+  $mail->Password = "P4rt1sj0n132"; // GMAIL password
+}else{
+  $email_from = 'no-reply@hiof.no';
+  $name_from = 'no-reply hiof';
+
+  $mail->IsSMTP();
+  $mail->SMTPAuth = false;
+  $mail->SMTPSecure = "ssl";
+  $mail->Host = "smtp.hiof.no";
+  $mail->Port = 465;
+  $mail->Username = "";
+  $mail->Password = "";
 }
+
 //Typical mail data
 $mail->AddAddress($email, $name);
 $mail->SetFrom($email_from, $name_from);
-$mail->Subject = "Snasen kaffe";
-$mail->Body = "Kreft smaker godt";
+$mail->Subject = "[Elbil] Lag nytt passord";
+$mail->Body = "Sjokolade smaker godt";
 
 try{
    $mail->Send();
-   echo "Success! \r\nE=" . $mail->ErrorInfo;
+   echo "Success! <br>" . $mail->ErrorInfo;
 } catch(Exception $e){
 //Something went bad
   echo "Fail - " . $mail->ErrorInfo;
