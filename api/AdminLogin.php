6087d381afd0f4f5c618bf566d9a74591b58ecc8
Jonas <mail@jonaskf.net>
2016-08-31 00:39:36 +0200

    login should work now with absolute minimal return data.

--- api/AdminLogin.php
+++ api/AdminLogin.php
@@ -10,42 +10,46 @@ session_start();
 
 if (!isset($_SESSION['user_id'])) {
   $_SESSION['sessionId'] = uniqid();
-  $_SESSION['username'] = strtolower($_POST['username']);
+  $_SESSION['username'] = strtolower($_GET['username']);
   $_SESSION['logged_in'] = false;
   //Getting userID
-  $sqlLogin = "select * from ec_user where username='" . filter_var($_SESSION['username'], FILTER_SANITIZE_STRING) . "'";
+  $sqlLogin = "select * from ec_user where username='" . filter_var($_SESSION['username'], FILTER_SANITIZE_STRING) . "';";
   $result_login = $conn->query($sqlLogin);
   if ($result_login->num_rows > 0) {
     // output data of each row
     while ($row = $result_login->fetch_assoc()) {
       $_SESSION['user_id'] = $row['user_id'];
       $result['username'] = $row['username'];
+      $isAdmin = $row['admin'];
       $hash = $row['password'];
     }
-    if(password_verify($_POST['password'], $hash) && $row['admin'] === 1){
+    if(password_verify($_GET['password'], $hash) && $isAdmin == 1){
       //The user is a admin and typed in the correct password
       $_SESSION['logged_in'] = true;
       echo json_encode($result);
     }else{
-      echo '';
+      echo 's';
     }
   }else {
     echo json_encode($result);
   }
 }else if($_SESSION['logged_in']){
-  $result_login = $conn->query("select settings from ec_user where user_id='" . filter_var($_SESSION['user_id'], FILTER_SANITIZE_STRING) . "';");
+  $sql = "select * from ec_user where user_id='" . filter_var($_SESSION['user_id'], FILTER_SANITIZE_STRING) . "';";
+
+  $result_login = $conn->query($sql);
   if ($result_login->num_rows > 0) {
     // output data of each row
     while ($row = $result_login->fetch_assoc()) {
-      if($row['admin'] === 1){
+      if($row['admin'] == 1){
         //The user is a admin and was already logged in
         $_SESSION['logged_in'] = true;
         $result['username'] = $row['username'];
-        echo json_encode($result);
       }else{
-        echo json_encode($result);
+        echo '404';
       }
     }
+
+    echo json_encode($result);
   }
 }else{
   session_destroy();
